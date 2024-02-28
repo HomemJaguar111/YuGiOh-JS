6f651e447fb857da170cc86778e8dc8de5e81d7c
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2024-02-28 12:50:25 -0300

    Falta pouco

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,24 +5,26 @@ const state = {
         scoreBox: document.getElementById("score_points")
     },
     cardSprites:{
-        avatar: document.getElementById("card-name"),
-       type: document.getElementById("card-type")
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type")
     },
     fieldCards:{
-        player:document.getElementById("player_field-card"),
-        computer:document.getElementById("computer_field-card")
+        player:document.getElementById("player-field-card"),
+        computer:document.getElementById("computer-field-card")
+    },
+    playerSides:{
+        player1:"player-cards",
+        player1BOX:document.querySelector("#player-cards"),
+        computer:"computer-cards",
+        computerBOX: document.querySelector("#computer-cards")
     },
     actions:{
         button:document.getElementById("next-duel")
     }    
 }
 
-const playerSides = {
-    player1:"player-field-card",
-    computer:"computer-field-card"
-}
-
-const pathImages = ".src/assets/icons/"
+const pathImages = "./src/assets/icons/"
 
 const cardData = [
     {
@@ -31,7 +33,7 @@ const cardData = [
         type:"Paper",
         img:`${pathImages}dragon.png`,
         WinOf:[1],
-        loseOf:[2],
+        LoseOf:[2],
     },
     {
         id:1,
@@ -39,7 +41,7 @@ const cardData = [
         type:"Rock",
         img:`${pathImages}magician.png`,
         WinOf:[2],
-        loseOf:[0],
+        LoseOf:[0],
     },
     {
         id:2,
@@ -47,22 +49,78 @@ const cardData = [
         type:"Scissors",
         img:`${pathImages}exodia.png`,
         WinOf:[0],
-        loseOf:[1],
+        LoseOf:[1],
     },
 ]
 
-async function drawCards(cardNumber, fieldSide){
+async function getRandomCardId(){
+    const randomIndex = Math.floor(Math.random() * cardData.length)
+    return cardData[randomIndex].id
+}
+
+async function createCardImage(IdCard, fieldSide){
+    const cardImage = document.createElement("img")
+    cardImage.setAttribute("height","100px")
+    cardImage.setAttribute("src","./src/assets/icons/card-back.png")
+    cardImage.setAttribute("data-id", IdCard)
+    cardImage.classList.add("card")
+
+    if(fieldSide=== state.playerSides.player1){      
+        cardImage.addEventListener("mouseover", ()=>{
+        drawSelectCard(IdCard)})
+        
+    cardImage.addEventListener("click", ()=>{
+        setCardsField(cardImage.getAttribute("data-id"))
+       } )
+    }
+
+    return cardImage 
+}
+
+async function setCardsField(cardId){
+    await removeAllCardsImages()
+
+    let computerCardId = await getRandomCardId()
+
+    state.fieldCards.player.style.display = "block"
+    state.fieldCards.computer.style.display = "block"
+
+    state.fieldCards.player.src = cardData[cardId].img
+    state.fieldCards.computer.src = cardData[computerCardId].img
+
+    let duelResults = await checkDuelResults(cardId, computerCardId)
+
+    //await updateScore()
+    //await drawButton(duelResults)
+} 
+
+async function removeAllCardsImages(){
+    let {computerBOX, player1BOX} = state.playerSides
+    let imgElements = computerBOX.querySelectorAll("img")
+    imgElements.forEach((img)=>img.remove())
+
+    imgElements = player1BOX.querySelectorAll("img")
+    imgElements.forEach((img)=>img.remove())
+}
+
+async function drawSelectCard(index){
+    state.cardSprites.avatar.src = cardData[index].img
+    state.cardSprites.name.innerText = cardData[index].name
+    state.cardSprites.type.innerText = "Attribute : " + cardData[index].type
+}
+
+async function drawCards(cardNumbers, fieldSide){
     for(let  i = 0; i<cardNumbers; i++){
         const randomIdCard = await getRandomCardId()
         const cardImage = await createCardImage(randomIdCard,fieldSide)
-
-        document.getElementById(fieldside.appendChild(cardImage))
+       
+        document.getElementById(fieldSide).appendChild(cardImage)
     }
 }
 
 function init(){
-    drawCards(5, playerSides.player1) 
-    drawCards(5, playerSides.computer) 
+    drawCards(5, state.playerSides.player1) 
+    drawCards(5, state.playerSides.computer) 
 }
 
 init()
